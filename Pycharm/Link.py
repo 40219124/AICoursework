0b42c778f165a01037ba8f0599d9045a08beb576
Tim-Surface <40219124@live.napier.ac.uk>
2018-03-11 02:56:17 +0000

    Made link simpler.  Removed distance calculation.

--- Pycharm/Link.py
+++ Pycharm/Link.py
@@ -1,6 +1,3 @@
-from Node import Node
-
-
 class Link:
 
     def __init__(self):
@@ -29,8 +26,3 @@ class Link:
     def set_ends(self, s, e):
         self.start = s
         self.end = e
-        self.weight = self.calculate_link_length(Node.nodes[s].get_position(), Node.nodes[e].get_position())
-
-    @staticmethod
-    def calculate_link_length(s, e):
-        return ((e[0] - s[0])**2 + (e[1] - s[1])**2)**(1/2.0)
