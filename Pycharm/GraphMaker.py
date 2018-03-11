ace2013eac31439749ebd9bf98a54c90934bbb2e
Tim-Surface <40219124@live.napier.ac.uk>
2018-03-11 02:59:57 +0000

    A class to make the nodes and connections called GraphMaker. Uses data stored on the static values in file input and stores the created nodes in its own static variable. Link length calculation moved to here. Main now utilises appropriate methods to create the list of nodes and their links from the data files.

---
+++ Pycharm/GraphMaker.py
@@ -0,0 +1,41 @@
+from FileInput import FileInput
+from Node import Node
+from Link import Link
+
+
+class GraphMaker:
+
+    nodes = []
+
+    def __init__(self):
+        self.x = 1
+
+    @staticmethod
+    def build_nodes():
+        nodes = []
+        nums = FileInput.numbers.copy()
+        nums = nums[1:nums[0] * 2 + 1]
+        while len(nums) > 0:
+            n = Node()
+            n.set_position((nums.pop(0), nums.pop(0)))
+            nodes.append(n)
+            print(str(n))
+        GraphMaker.nodes = nodes.copy()
+
+    @staticmethod
+    def build_connections():
+        count = FileInput.count
+        nums = FileInput.numbers.copy()
+        cons = nums[FileInput.count * 2 + 1:]
+        for f in range(7):
+            for t in range(7):
+                if cons[count * t + f] == 1:
+                    link = Link()
+                    link.set_ends(f, t)
+                    link.set_length(GraphMaker.calculate_link_length(
+                        Node.nodes[f].get_position(), Node.nodes[t].get_position()))
+                    GraphMaker.nodes[f].add_connection(link)
+
+    @staticmethod
+    def calculate_link_length(s, e):
+        return ((e[0] - s[0])**2 + (e[1] - s[1])**2)**(1/2.0)
--- Pycharm/main.py
+++ Pycharm/main.py
@@ -1,5 +1,17 @@
 from FileInput import FileInput
+from Node import Node
+from GraphMaker import GraphMaker
 
 # run program
 print("Hello world.")
-fi = FileInput()
+fi = FileInput.do_file_input()
+GraphMaker.build_nodes()
+GraphMaker.build_connections()
+for n in GraphMaker.nodes:
+    print(str(n))
+n = Node()
+n.set_position((1, 2))
+print(str(n))
+n2 = Node()
+n2.set_position((4, 3))
+print(str(n2))
