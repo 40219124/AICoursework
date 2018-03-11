2cfddcd74a614832e2890dc11737de01458b4736
Tim-Surface <40219124@live.napier.ac.uk>
2018-03-11 02:54:26 +0000

    Changes to make FileInput static.

--- Pycharm/FileInput.py
+++ Pycharm/FileInput.py
@@ -3,30 +3,37 @@ import os
 
 class FileInput:
 
-    def __init__(self):
-        self.file_data = ""
-        self.count = 0
-        self.numbers = []
-        self.read_file()
-        self.process_data()
-        self.print_file()
+    file_data = ""
+    count = 0
+    numbers = []
 
-    def read_file(self):
+    @staticmethod
+    def do_file_input():
+        FileInput.read_file()
+        FileInput.process_data()
+        FileInput.print_file()
+
+    @staticmethod
+    def read_file():
         this_dir = os.path.dirname(__file__)
         rel_path = "..\Caverns\input1.cav"
         abs_path = os.path.join(this_dir, rel_path)
         with open(abs_path, 'r') as f:
-            self.file_data = f.read()
+            FileInput.file_data = f.read()
 
-    def process_data(self):
-        sep = self.file_data.split(',')
-        self.count = int(sep[0])
+    @staticmethod
+    def process_data():
+        if len(FileInput.numbers) > 0:
+            FileInput.numbers.clear()
+        sep = FileInput.file_data.split(',')
+        FileInput.count = int(sep[0])
         for val in sep:
-            self.numbers.append(int(val))
+            FileInput.numbers.append(int(val))
 
-    def print_file(self):
-        self.process_data()
-        print(self.numbers[:1])
-        print(self.numbers[1:1 + self.count * 2])
-        for i in range(self.count):
-            print(self.numbers[1 + self.count * 2 + self.count * i:1 + self.count * 2 + self.count * (i+1)])
+    @staticmethod
+    def print_file():
+        print(FileInput.numbers[:1])
+        print(FileInput.numbers[1:1 + FileInput.count * 2])
+        for i in range(FileInput.count):
+            print(FileInput.numbers[1 + FileInput.count * 2 + FileInput.count * i:
+                                    1 + FileInput.count * 2 + FileInput.count * (i+1)])
