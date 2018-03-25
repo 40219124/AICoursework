dcfa43fb492de2814e71387c28b9ee05e83f4d86
Tim-Surface <40219124@live.napier.ac.uk>
2018-03-25 23:56:56 +0100

    Test set up of a window with some buttons.

--- Pycharm/Renderer.py
+++ Pycharm/Renderer.py
@@ -1,8 +1,31 @@
 from tkinter import *
 import tkinter as tk
+from Solver import Solver
 
 
 class Renderer:
 
     def __init__(self):
-        s = 0
+        self.window = Tk()
+        self.window.title("Hey!")
+        self.window.geometry("300x300")
+
+        frame = Frame(self.window)
+        frame.pack()
+
+        bottomframe = Frame(self.window)
+        bottomframe.pack(side=BOTTOM)
+
+        redbutton = Button(bottomframe, text="Red", fg="red")
+        redbutton.pack(side=RIGHT)
+
+        bluebutton = Button(bottomframe, text="Blue", fg="blue")
+        bluebutton.pack(side=LEFT)
+
+        greenbutton = Button(bottomframe, text="Green", fg="green")
+        greenbutton.pack(side=TOP)
+
+        blackbutton = Button(bottomframe, text="Black", fg="black", command=Solver.generate_path)
+        blackbutton.pack(side=BOTTOM)
+
+        self.window.mainloop()
